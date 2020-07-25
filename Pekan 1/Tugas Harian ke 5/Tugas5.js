4e4702fd54ba162a482ddd947e883ceb2dc156ca
digitradiart <digitradiart@gmail.com>
2020-07-25 07:15:59 +0700

    perbaiki jawaban

--- Pekan 1/Tugas Harian ke 5/Tugas5.js
+++ Pekan 1/Tugas Harian ke 5/Tugas5.js
@@ -1,56 +1,33 @@
-/* ///////////////////////////////////////////////////////////////////////
-SOAL 1
-Tulislah sebuah function dengan nama halo() yang mengembalikan nilai “Halo Sanbers!” yang kemudian dapat ditampilkan di console.
- //   Tulis code function di sini
-console.log(halo()) // "Halo Sanbers!"  
-*/
+///////////////////////////////////////////////////////////////////////
+// SOAL 1
 console.log('---SOAL 1---')
 function halo(){
     console.log('Halo Sanbers!')
 }
-halo();
+halo()
 
-/* ///////////////////////////////////////////////////////////////////////
-SOAL 2
-Tulislah sebuah function dengan nama kalikan() yang mengembalikan hasil perkalian dua parameter yang di kirim.
-// Tulis code function di sini
-var num1 = 12
-var num2 = 4
- 
-var hasilKali = kalikan(num1, num2)
-console.log(hasilKali) // 48
-*/
-
+///////////////////////////////////////////////////////////////////////
+// SOAL 2
 console.log('\n---SOAL 2---')
 function kalikan(num1, num2){
     return num1 * num2
 }
-
-var num1 = 12
-var num2 = 4
-var hasilKali = kalikan(num1, num2)
-console.log(hasilKali)
+console.log(kalikan(12,4))
 
 
-/* ///////////////////////////////////////////////////////////////////////
-SOAL 3
-Tulislah sebuah function dengan nama introduce() yang memproses paramater yang dikirim menjadi sebuah kalimat perkenalan seperti berikut: “Nama saya [name], umur saya [age] tahun, alamat saya di [address], dan saya punya hobby yaitu [hobby]!”
+///////////////////////////////////////////////////////////////////////
+// SOAL 3
 
-// Tulis code function di sini
-var name = "John"
-var age = 30
-var address = "Jalan belum jadi"
-var hobby = "Gaming"
- 
-var perkenalan = introduce(name, age, address, hobby)
-console.log(perkenalan) // Menampilkan "Nama saya John, umur saya 30 tahun, alamat saya di jalan belum jadi, dan saya punya hobby yaitu Gaming!" 
-*/
 console.log('\n---SOAL 3---')
+
+console.log('---cara 1---')
 function introduce(name, age, address, hobby){
     console.log(`Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`)
 }
-var name = "DIGITRADIART"
-var age = 24
-var address = "West Java"
-var hobby = "Design & Code"
-introduce(name, age, address, hobby)
+console.log(introduce('DIGITRADIART', 24, 'West Java', 'Design & Code'))
+
+console.log('\n---cara 2---')
+function introduce(name, age, address, hobby){
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`
+}
+console.log(introduce('DIGITRADIART', 24, 'West Java', 'Design & Code'))
